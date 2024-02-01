b666ed7fbc5c4d01add054466483d31a81817aa4
jonny <jonathan.heindl@brandad-systems.de>
2024-02-01 13:47:35 +0100

    fixed inital start and screen scaling issue

--- src/util/screens.ts
+++ src/util/screens.ts
@@ -14,27 +14,60 @@ export interface Screen {
 
 export function getScreenLayout() {
     if (platform() == "win32") {
-        const powerShellCommnad = `Add-Type -AssemblyName System.Windows.Forms ; ([System.Windows.Forms.Screen]::AllScreens|sort -Property {$_.Displayname} )`
+        // |sort -Property {$_.Displayname}
+        const powerShellCommnad = `Add-Type -AssemblyName System.Windows.Forms ; ([System.Windows.Forms.Screen]::AllScreens ) |ConvertTo-Json`
 
         const output = execSync(powerShellCommnad, {
             encoding: "utf8",
             shell: "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
         })
-        const monitorStrings = output.trim().split("\r\n\r\n")
-
-        return monitorStrings.map(monitorStr => {
-            const boundsMatch = (/Bounds       : {X=(?<xccoord>-?\d*),Y=(?<yccoord>-?\d*),Width=(?<width>\d*),Height=(?<height>\d*)}/gm).exec(monitorStr)
-            const isPrimary = (/Primary\s*:\s*(?<isPrimary>(True|False))/).exec(monitorStr)?.groups?.isPrimary
-
-            if (!boundsMatch?.groups) {
-                return null
+        const screenInfo = JSON.parse(output) as Array<{
+            DeviceName: string
+            Primary: boolean,
+            WorkingArea: {
+                X: number,
+                Y: number
+                Width: number
+                Height: number
+            },
+            Bounds: {
+                X: number,
+                Y: number
+                Width: number
+                Height: number
             }
+        }>
+
+
+        return screenInfo.map(monitorStr => {
+
+            const deviceNr = monitorStr.DeviceName.split("DISPLAY")[1]
+            const scaledoutput = execSync(`Get-WmiObject win32_desktopmonitor| Where-Object -Property DeviceID -eq DesktopMonitor${deviceNr} |ConvertTo-Json`, {
+                encoding: "utf8",
+                shell: "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
+            })
+            if (scaledoutput.trim()) {
+                const scaledObj = JSON.parse(scaledoutput) as {
+                    ScreenHeight: number
+                    ScreenWidth: number
+                }
+                const scaleFactor = scaledObj.ScreenWidth / monitorStr.Bounds.Width
+                if (scaleFactor && Math.floor(scaleFactor * monitorStr.Bounds.Height) === scaledObj.ScreenHeight) {
+                    const newWidth = Math.floor(scaleFactor * monitorStr.WorkingArea.Width)
+                    const widthDiff = newWidth - monitorStr.WorkingArea.Width
+                    monitorStr.WorkingArea.Width = Math.floor(scaleFactor * monitorStr.WorkingArea.Width)
+                    monitorStr.WorkingArea.Height = Math.floor(scaleFactor * monitorStr.WorkingArea.Height)
+                    monitorStr.WorkingArea.X -= widthDiff
+                }
+
+            }
+
             return {
-                isPrimary: isPrimary === "True",
-                x: +boundsMatch?.groups?.xccoord,
-                y: +boundsMatch?.groups?.yccoord,
-                width: +boundsMatch?.groups?.width,
-                height: +boundsMatch?.groups?.height,
+                isPrimary: monitorStr.Primary,
+                x: monitorStr.WorkingArea.X,
+                y: monitorStr.WorkingArea.Y,
+                width: monitorStr.WorkingArea.Width,
+                height: monitorStr.WorkingArea.Height,
             } as Screen
         })
     }
@@ -61,6 +94,9 @@ export function enterMAxMAxScreenLAyout(app: BrowserWindow) {
             const previousSetup = yMap[screen.y]
             if (previousSetup.x + previousSetup.width === screen.x) {
                 previousSetup.width += screen.width
+            } else if (previousSetup.x - screen.width == screen.x) {
+                previousSetup.width += screen.width
+                previousSetup.x = screen.x
             }
         } else {
             yMap[screen.y] = screen
